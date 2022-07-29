1b055862c9d552b42100e8b5e43ed2e3eaa0c1b0
Jason Colwell <colwell3@illinois.edu>
2022-07-29 14:48:51 -0500

    add beta scripts

---
+++ bin/add-issues-from-jira-into-issues-github.ps1
@@ -0,0 +1,40 @@
+#Prerequisite modules
+# JiraPS: https://atlassianps.org/module/JiraPS/
+# GIthub Module: https://github.com/Microsoft/PowerShellForGitHub
+#Example usage
+# New-JiraSession -credential netid
+# .\add-issues-from-jira-issues-github -ownername uiuclibrary -repositoryname feed-business-office -JIRAProject BINF
+
+param ( $OwnerName, $RepositoryName, $JIRAProject )
+
+$issues = Get-JiraIssue -Query "project = $JIRAProject" | select-object summary, status, Description
+
+foreach ($issue in $issues) {
+    
+    if ($issue.Status -eq "Closed" -or $issue.Status -eq "Resolved" -or $issue.Status -eq "Done") { $issuestatus = "Closed" }
+    elseif ($issue.Status -eq "Open" -or $issue.Status -eq "To Do" -or $issue.Status -eq "Backlog" -or $issue.Status -eq "Selected for Development" -or $issue.status -eq "In Progress") { $issuestatus = "Open" }
+    else {$issuestatus = "Open"}
+    
+    $summary = $Issue.summary | out-string
+    $description = $issues.Description | out-string
+    # $Issue = get-githubissue -OwnerName $OwnerName -RepositoryName $RepositoryName | where-object title -like $summary 
+    
+    if ($issue = get-githubissue -OwnerName $OwnerName -RepositoryName $RepositoryName | where-object title -like $summary) {
+        write-host "Issue already created at: $($issue.html_url)"
+    }
+    elseif ($issue = get-githubissue -OwnerName $OwnerName -RepositoryName $RepositoryName -State closed | where-object title -like $summary) {
+        write-host "Issue already created at: $($issue.html_url)"
+    }
+    else{
+        # write-host "creating issue"
+        try {
+            Get-GitHubRepository -OwnerName $OwnerName -RepositoryName $RepositoryName | New-GitHubIssue -Title $summary -Body $description
+        }
+        catch {
+            start-sleep -Seconds 60
+            Get-GitHubRepository -OwnerName $OwnerName -RepositoryName $RepositoryName | New-GitHubIssue -Title $summary -Body $description
+        }
+    }
+
+    Get-Githubissue -OwnerName $OwnerName -RepositoryName $RepositoryName | Where-Object title -like $summary | Set-GitHubIssue -State $issuestatus
+}
---
+++ bin/add-issues-from-jira-to-project-classic-cards.ps1
@@ -0,0 +1,30 @@
+#Prerequisite modules
+# JiraPS: https://atlassianps.org/module/JiraPS/
+# GIthub Module: https://github.com/Microsoft/PowerShellForGitHub
+#Example usage
+# New-JiraSession -credential netid
+
+# .\add-cards-from-jira.ps1 -ownername uiuclibrary -repositoryname feed-business-office -JIRAProject BINF
+
+param ( $OwnerName, $RepositoryName, $JIRAProject )
+
+$issues = Get-JiraIssue -Query "project = $JIRAProject" | select-object summary, status, Description
+
+foreach ($issue in $issues) {
+    
+    if ($issue.Status -eq "Closed" -or $issue.Status -eq "Resolved" -or $issue.Status -eq "Done") { $ProjectColumn = "Done" }
+    elseif ($issue.Status -eq "Open" -or $issue.Status -eq "To Do" -or $issue.Status -eq "Backlog") { $ProjectColumn = "To Do" }
+    elseif ($issue.Status -eq "Selected for Development" -or $issue.status -eq "In Progress") {$ProjectColumn = "In Progress"}
+    else {$StatusUnknown}
+    
+    #I don't think this is necessary, but left it in anyways.
+    $summary = $Issue.summary | out-string
+
+    if ($StatusUnknown) {Write-Output "Status is $issue.status which is not mapped to a value in the script."}
+    elseif ($ProjectColumn -ne "Done") {
+        
+    }
+    elseif ($ProjectColumn -eq "Done"){ }
+    Get-GitHubProject -OwnerName $OwnerName -RepositoryName $RepositoryName | Get-GitHubProjectColumn | where-object { $_.name -eq $ProjectColumn } | New-GitHubProjectCard -Note $summary
+    }
+}
